9db287b7a1957f850d123353509449a0ad79e283
KrieqqeR <burakcevizli@gmail.com>
2023-09-20 13:44:15 +0300

    Homework is done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person person = new Person("John", "Doe",13);
+        System.out.println("First name : " + person.getFirstName());
+        System.out.println("Last name : " + person.getLastName());
+        System.out.println(" Age : " + person.getAge());
+        System.out.println("isTeen : " + person.isTeen());
+        System.out.println("*******************************");
+
+        Wall wall = new Wall(5,4);
+
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        wall.setWidth(2.4);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
+
+
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,37 @@
+public class Person {
+
+    String firstName;
+    String lastName;
+    int age;
+    String education;
+    int id;
+    int heigth;
+
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(String education, int id, int heigth){
+        this.education=education;
+        this.id=id;
+        this.heigth=heigth;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public boolean isTeen (){
+        return age < 20 && age > 12;
+    }
+
+}
